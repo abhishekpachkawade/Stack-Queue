6ca64a31d1538b2413b2ced28d273ac4109f4bc0
abhishekpachkawade <abhishekpachkawade@gmail.com>
2021-09-29 19:53:45 +0530

    First commit

---
+++ StackAndQueue.java
@@ -0,0 +1,10 @@
+package com.stackqueue;
+
+public class StackAndQueue {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Data Structure -Stacks and Queues problem");
+
+	}
+
+}
